99b24ff1a990eea93f53df9b025449c344053a48
lyeow <e0941684@u.nus.edu>
2022-07-26 10:53:46 +0800

    first push

---
+++ team5_vms/src/main/java/iss/team5/vms/Team5VmsApplication.java
@@ -0,0 +1,13 @@
+package iss.team5.vms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5VmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5VmsApplication.class, args);
+	}
+
+}
---
+++ team5_vms/src/test/java/iss/team5/vms/Team5VmsApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.team5.vms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5VmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
