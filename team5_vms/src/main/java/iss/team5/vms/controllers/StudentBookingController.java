0975860c8b1d96d65cc25e90e5ba7e444f2b601f
YI Ling <e0941863@u.nus.edu>
2022-07-27 09:59:22 +0800

    27/7 push2

--- team5_vms/src/main/java/iss/team5/vms/controllers/StudentBookingController.java
+++ team5_vms/src/main/java/iss/team5/vms/controllers/StudentBookingController.java
@@ -1,13 +1,20 @@
 package iss.team5.vms.controllers;
 
+import java.util.ArrayList;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import iss.team5.vms.model.Booking;
 import iss.team5.vms.services.BookingService;
+import iss.team5.vms.services.StudentService;
 ;
 
 @Controller
@@ -16,12 +23,32 @@ public class StudentBookingController {
 	
 	@Autowired
 	private BookingService bService;
+	@Autowired
+	private StudentService sService;
 	
 	
 	@RequestMapping(value = "/new", method = RequestMethod.GET)	
 	public ModelAndView newBooking() {
 		ModelAndView mav = new ModelAndView("new_booking", "booking", new Booking());
-		Student 
+		Student student = 
+		ArrayList<Booking> bList = bService.findAllBookings();
+		mav.addObject(bList);
+		return mav;
+		
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.POST)	
+	public ModelAndView createNewBooking(@ModelAttribute @Valid Booking booking,
+			BindingResult result) {
+		if(result.hasErrors())
+			return new ModelAndView("new_booking");
+		
+		ModelAndView mav = new ModelAndView();
+		String message = "New booking " + booking.getId() + " was successfully created.";
+		System.out.println(message);
+		bService.createBooking(booking);
+		mav.setViewName("forward:/book");
+		return mav;
 		
 	}
 		
